19adeaf5d375a04d1e38a9aa2d1ffb6980e21470
evan <evan.ferron53@gmail.com>
2025-02-18 10:04:33 +0100

    🧪 | add test files to implements

--- src/core/base/AController.ts
+++ src/core/base/AController.ts
@@ -8,19 +8,4 @@ export abstract class AController {
     constructor() {
         this.config = Config.getInstance();
     }
-
-    // /**
-    //  * Method that will convert a database entry to a response model
-    //  * 
-    //  * This method wil convert an entry to a response model
-    //  * If you want to convert a list of entries to a response model you can use this method in a map function
-    //  *  
-    //  * @param entry
-    //  */
-    // protected toModel<EntryType extends IEntry, ResponseType extends IResponse>(entry: EntryType): SuccessResponse<ResponseType> {
-    //     return {
-    //         success: true,
-    //         data: entry as unknown as ResponseType,
-    //     };
-    // }
 }
