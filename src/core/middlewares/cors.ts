5e5ab6adbc3b05ac576f6834991cdf87c8ffc929
matiasbellaud <bellaud.matias@gmail.com>
2025-04-23 11:59:29 +0200

    fix | cors policy error

--- src/core/middlewares/cors.ts
+++ src/core/middlewares/cors.ts
@@ -1,16 +1,23 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 
-// TODO : | delete the localhost url when the project is deployed
 const allowedOrigins = [process.env.FRONTEND_URL, "http://localhost:5173"];
 
-export const checkOrigin = (req: Request, res: Response, next: NextFunction) => {
+export const checkOrigin = (req: Request, res: Response, next: NextFunction): void => {
     const origin = req.headers.origin;
 
-    // allow request that doesn't have origin (postman) and allowed origins
     if (!origin || allowedOrigins.includes(origin)) {
-        next();
+        res.header("Access-Control-Allow-Origin", origin || "*");
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, x-api-key");
+        res.header("Access-Control-Allow-Credentials", "true");
+
+        if (req.method === "OPTIONS") {
+            res.sendStatus(200);
+        } else {
+            next();
+        }
     } else {
         res.status(403).json({ error: "Forbidden: Unauthorized origin" });
     }
-
 };
