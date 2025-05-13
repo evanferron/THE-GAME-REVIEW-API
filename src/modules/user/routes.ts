0a54951291880b825af1cf00794d93ca92a29ff1
matiasbellaud <bellaud.matias@gmail.com>
2025-05-13 15:21:45 +0200

    fix | user deleted_at Date type

--- src/modules/user/routes.ts
+++ src/modules/user/routes.ts
@@ -9,7 +9,7 @@ export function createUserRoutes(): Router {
     router.get("/", authMiddleware, userController.getUserById);
     router.put("/", authMiddleware, userController.updateUser);
     router.put("/info", authMiddleware, userController.updateUserInfo);
-     router.put("/password", authMiddleware, userController.updateUserPassword);
+    router.put("/password", authMiddleware, userController.updateUserPassword);
     router.delete("/", authMiddleware, userController.deleteUser);
 
     return router;
