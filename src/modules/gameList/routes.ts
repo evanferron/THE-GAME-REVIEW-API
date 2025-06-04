7f753cd36208414ae662b04a78ea87f75737ae59
matiasbellaud <101833344+matiasbellaud@users.noreply.github.com>
2025-06-04 15:36:02 +0200

    Fix/get game list (#16)
    * fix get game list by name
    
    * fix | get game list by name
    
    * fix | change name function + delete console log
    
    * fix | add developper to the return

--- src/modules/gameList/routes.ts
+++ src/modules/gameList/routes.ts
@@ -9,6 +9,7 @@ export function createGameListRoutes(): Router {
     router.post("/", authMiddleware, gameListController.createGameList);
     router.get("/", authMiddleware, gameListController.getAllGamesLists);
     router.get("/get_by_list_id", authMiddleware, gameListController.getGamesListsByListId);
+    router.get("/name", authMiddleware, gameListController.getGamesListsByName);
     router.delete("/", authMiddleware, gameListController.deleteGameList);
 
     return router;
