96fd9532674a6c4db7813cc7881ee1aeefd57233
evan <evan.ferron53@gmail.com>
2025-06-06 11:33:29 +0200

    feat(liked_review): add somes commments

--- src/core/middlewares/cors.ts
+++ src/core/middlewares/cors.ts
@@ -2,21 +2,22 @@ import { Request, Response, NextFunction } from "express";
 
 const allowedOrigins = [process.env.FRONTEND_URL, "http://localhost:5173"];
 
+
 /**
- * Middleware to check the request's origin against a list of allowed origins and set appropriate CORS headers.
+ * Middleware to check the request's origin and API key for CORS and authentication.
  *
- * - If the request's origin is allowed or not specified, sets CORS headers to allow the request.
- * - Handles preflight OPTIONS requests by responding with status 200.
- * - If the origin is not allowed, responds with a 403 Forbidden error.
+ * - Verifies the presence of an `x-api-key` header; responds with 401 if missing.
+ * - Checks if the request's origin is allowed (or missing); sets appropriate CORS headers.
+ * - Handles preflight `OPTIONS` requests by responding with 200.
+ * - Calls `next()` for valid requests; otherwise responds with 403 for unauthorized origins.
  *
- * @param req - The Express request object.
- * @param res - The Express response object.
- * @param next - The next middleware function in the stack.
+ * @param req - Express request object.
+ * @param res - Express response object.
+ * @param next - Express next middleware function.
  */
 export const checkOrigin = (req: Request, res: Response, next: NextFunction): void => {
     const origin = req.headers.origin;
 
-    // Vérification de la présence de la clé API dans l'en-tête
     const apiKey = req.headers['x-api-key'];
     if (!apiKey) {
         res.status(401).json({ error: "Unauthorized: API key missing" });
