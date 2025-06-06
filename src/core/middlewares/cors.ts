91c2429d335c7a6d673884e254178a3dda8e3b93
evan <evan.ferron53@gmail.com>
2025-06-06 11:32:51 +0200

    feat(liked_review): add somes commments

--- src/core/middlewares/cors.ts
+++ src/core/middlewares/cors.ts
@@ -16,10 +16,16 @@ const allowedOrigins = [process.env.FRONTEND_URL, "http://localhost:5173"];
 export const checkOrigin = (req: Request, res: Response, next: NextFunction): void => {
     const origin = req.headers.origin;
 
+    // Vérification de la présence de la clé API dans l'en-tête
+    const apiKey = req.headers['x-api-key'];
+    if (!apiKey) {
+        res.status(401).json({ error: "Unauthorized: API key missing" });
+        return;
+    }
+
     if (!origin || allowedOrigins.includes(origin)) {
-        res.header("Access-Control-Allow-Origin", origin || "*");
+        res.header("Access-Control-Allow-Origin", origin ?? "*");
         res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
         res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, x-api-key");
         res.header("Access-Control-Allow-Credentials", "true");
 
