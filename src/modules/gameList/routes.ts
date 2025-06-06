873677af59e9e0db6fc1f31be341484dcf4364c2
matiasbellaud <bellaud.matias@gmail.com>
2025-06-06 15:59:16 +0200

    fix | fix bug from getGameListName request

--- src/modules/gameList/routes.ts
+++ src/modules/gameList/routes.ts
@@ -8,8 +8,8 @@ export function createGameListRoutes(): Router {
 
     router.post("/", authMiddleware, gameListController.createGameList);
     router.get("/", authMiddleware, gameListController.getAllGamesLists);
-    router.get("/get_by_list_id", authMiddleware, gameListController.getGamesListsByListId);
-    router.get("/name", authMiddleware, gameListController.getGamesListsByName);
+    router.post("/get_by_list_id", authMiddleware, gameListController.getGamesListsByListId);
+    router.post("/name", authMiddleware, gameListController.getGamesListsByName);
     router.delete("/", authMiddleware, gameListController.deleteGameList);
 
     return router;
